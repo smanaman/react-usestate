0161799528ebf2ac3f5342c83ac1368ba165c841
smanaman <sanvatsman@gmail.com>
2025-01-09 10:22:17 +0530

    hooks

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,42 +1,54 @@
 import './Counter.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 function Counter() {
-   const [count,setcouter] = useState(0)
+  const [count, setcouter] = useState(1)
+  const [massege, setmessege] = useState('')
+  let max = 5;
+  useEffect(() => {
+    document.title = `Count : ${count}`
+    if (count == max) {
+      setmessege('Max limit')
+    }else{
+      setmessege('')
+    }
+  })
 
-   const incrimeant=()=>{
-    setcouter(count+1)
-   }
-   const dicrimeant=()=>{
-if(count<=0){
-    count==0
-}else{
-    setcouter(count-1)
-}
-   }
- return(
+  const incrimeant = () => {
+    setcouter(count + 1)
+  }
+  const dicrimeant = () => {
+    if (count <= 1) {
+      count == 0
+    } else {
+      setcouter(count - 1)
+    }
+  }
+  return (
     <>
 
-<div className="card">
-  <img src="https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg" alt="Card Image" />
-  <div className="card-content">
-    <h2>Card Title</h2>
-    <p>This is a simple, attractive card design using HTML and CSS.</p>
-    <div className="btnsection">
-    <button onClick={incrimeant}>+</button>  
-     <p className='p'>{count}</p>
-     <button onClick={dicrimeant}>-</button> 
-    </div>
-    
-     <br/>
-     <br/>
-    <a href="#" className="btn">
-      Learn More
-    </a>
-  </div>
-</div>
+      <div className="card">
+        <img src="https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg" alt="Card Image" />
+        <div className="card-content">
+          <h2>Card Title</h2>
+          <p>This is a simple, attractive card design using HTML and CSS.</p>
+          <div className="btnsection">
+          <button onClick={dicrimeant}>-</button>
+
+            <p className='p'>{count}</p>
+            <button onClick={incrimeant} disabled={count==max}>+</button>
+          </div>
+
+          <br />
+          <br />
+          <a href="#" className="btn">
+            Learn More
+          </a><br />
+          <p>{massege}</p>
+        </div>
+      </div>
 
     </>
- ) 
+  )
 
 }
 export default Counter
